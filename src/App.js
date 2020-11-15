613fc8e4c09f8dc07d8c908f2e4d996f6d0b3939
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-14 23:03:30 -0300

    attempting to render SearchBar in MovieLibrary

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import MovieLibrary from './components/MovieLibrary';
 import SearchBar from './components/SearchBar';
 
 import Header from './components/Header';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SearchBar />
-      <MovieLibrary />
+      <MovieLibrary movies={data} />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,12 +7,27 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor() {
+    super();
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar 
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+
+          
+        />
         <MovieList movies={this.props.movies} />
         <AddMovie />
       </div>
